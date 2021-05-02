a373d4aa307ebfda653ea81bb5c44b91070a2629
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-05-02 20:28:22 +0200

    essai gsp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
     // animation main section h2
+        // animations mes nouveaux amis
 
     let h2 = document.querySelectorAll('h2');
+    let aproposSpans = document.querySelectorAll('span');
     window.addEventListener('load', ()=>{
         const TL = gsap.timeline({paused:true});
         TL
-        .staggerFrom(h2, 5, {transform: "scale(3)", ease: "power2.out"}, '-=9')
+        .from(h2, 5, {transform: "scale(2)", ease: "power2.out"})
+        .from(aproposSpans, 5, {transform: "scale(2)", ease: "power2.out"})
          
         TL.play();
     
     })
+
+    
+   
