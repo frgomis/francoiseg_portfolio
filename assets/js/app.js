ad0c88abcf844ad05f479b413ab9e091a586f58f
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-05-05 12:15:04 +0200

    modif section.apropos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,13 @@
         // animations mes nouveaux amis
 
     let h2 = document.querySelectorAll('h2');
-    let aproposSpans = document.querySelectorAll('span');
+    let aproposSpans = document.querySelector('span');
     window.addEventListener('load', ()=>{
         const TL = gsap.timeline({paused:true});
         TL
         .from(h2, 5, {transform: "scale(2)", ease: "power2.out"})
         .from(aproposSpans, 5, {transform: "scale(2)", ease: "power2.out"})
+        gsap.to(aproposSpans, {duration: 2, text: "CSS3", ease: "none"});
          
         TL.play();
     
